aec203caed18beedac3eb94d708e5cb16f0bd7cb
Kevin W <kjoewill@gmail.com>
2018-08-30 17:28:15 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -15,8 +15,13 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   
   projects = {}
+  
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
  
-  binding.pry
+  projects
+ 
 end
 
 create_project_hash
